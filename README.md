281672510b4d11bd76bde8b05fbaccd220d576b6
cmh <47976997+withhold17@users.noreply.github.com>
2022-05-17 14:55:20 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# 操作指南
+
+**此SDK用于实验教学。**
+
+原作者：[链接](https://github.com/AlessioMorale/ld06_lidar.git)
+
+ROS版本：ROS melodic
+
+## 编译
+
+```bash
+# 切换到工作空间下
+$ cd ~/catkin_ws/src
+
+# 下载源码
+$ git clone https://github.com/withhold17/TeachingCar.git
+
+# 安装依赖及编译
+$ cd ..
+$ rosdep install --from-paths src --ignore-src -y
+$ catkin_make -DCATKIN_WHITELIST_PACKAGES="ld06_lidar"
+```
+
+
+
+## 运行
+
+```bash
+# 查看激光雷达数据（打开配置好的rviz）
+$ roslaunch ld06_lidar view_ld06.launch
+
+# 启动激光雷达
+$ roslaunch ld06_lidar ld06.launch
+```
---
+++ src/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include "cmd_interface_linux.h"
+#include <stdio.h>
+#include "lipkg.h"
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include "tofbf.h"
+#include <string>
+#include <math.h>
+
+
+int main(int argc , char **argv)
+{
+	ros::init(argc, argv, "ld06_lidar");
+	ros::NodeHandle nh;                    /* create a ROS Node */
+	ros::NodeHandle nh_private("~");
+ 	
+	LiPkg * lidar = new LiPkg;
+  
+	CmdInterfaceLinux cmd_port;
+	std::string port_name;
+	std::string lidar_frame;
+	float angle_max, angle_min;
+	float range_max, range_min;
+
+	nh_private.param<std::string>("lidar_frame", lidar_frame, "lidar_frame");
+	lidar->SetLidarFrame(lidar_frame);
+
+	nh_private.getParam("range_max", range_max);
+	nh_private.getParam("range_min", range_min);
+	nh_private.getParam("angle_max", angle_max);
+	nh_private.getParam("angle_min", angle_min);
+
+	lidar->SetRangeMax(range_max);
+	lidar->SetRangeMin(range_min);
+	lidar->SetAngleMax(angle_max);
+	lidar->SetAngleMin(angle_min);
+
+	nh_private.param<std::string>("serial_port", port_name, std::string());
+
+	if (port_name.empty())
+	{
+		ROS_INFO("Autodetecting serial port");
+		std::vector<std::pair<std::string, std::string>> device_list;
+		cmd_port.GetCmdDevices(device_list);
+		auto found = std::find_if(
+			device_list.begin(),
+			device_list.end(),
+			[](std::pair<std::string, std::string> n)
+			{ return strstr(n.second.c_str(), "CP2102"); });
+
+		if (found != device_list.end())
+		{
+			ROS_INFO_STREAM(found->first << "    " << found->second);
+			port_name = found->first;
+		}
+		else
+		{
+			ROS_ERROR("Can't find LiDAR LD06");
+			return -1;
+		}
+	}
+
+	ROS_INFO_STREAM("Using port " << port_name);
+
+	cmd_port.SetReadCallback([&lidar](const char *byte, size_t len) {
+		if(lidar->Parse((uint8_t*)byte, len))
+		{
+			lidar->AssemblePacket();
+		}
+	});
+
+	if(cmd_port.Open(port_name)){
+		ROS_INFO("LiDAR_LD06 started successfully");
+	} else {
+		ROS_ERROR("Can't open the serial port");
+		return -1;
+	}
+	ros::Publisher lidar_pub = nh.advertise<sensor_msgs::LaserScan>("LiDAR/LD06", 1); /*create a ROS topic */
+	
+	ros::Rate rate(30);
+	while (ros::ok())
+	{
+		if (lidar->IsFrameReady())
+		{
+			lidar_pub.publish(lidar->GetLaserScan());  // Fixed Frame:  lidar_frame
+			lidar->ResetFrameReady();
+		}
+		rate.sleep();
+	}
+    return 0;
+}
+
